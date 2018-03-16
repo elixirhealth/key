42562f417d3137aea0b883a96e360d27cd80ab7a
Drausin Wulsin <daedalus2718@gmail.com>
2018-03-16 13:40:53 -0400

    add client pkg (#13)

---
+++ pkg/client/client.go
@@ -0,0 +1,15 @@
+package client
+
+import (
+	api "github.com/elxirhealth/key/pkg/keyapi"
+	"google.golang.org/grpc"
+)
+
+// NewInsecure returns a new KeyClient without any TLS on the connection.
+func NewInsecure(address string) (api.KeyClient, error) {
+	cc, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	return api.NewKeyClient(cc), nil
+}
